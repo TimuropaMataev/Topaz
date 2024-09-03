2b1d78993dfbd59c50f7e4e95cdf6068094d7586
timurmataev <mataevtimur68@gmail.com>
2024-09-03 19:36:20 +0600

    bagfix last

--- Topaz/Models/User.cs
+++ Topaz/Models/User.cs
@@ -12,10 +12,10 @@ public class User
     [StringLength(40, MinimumLength = 2, ErrorMessage = "Диапазон от 2 до 40")]
     public string? Name { get; set; }
 
-    [Required(ErrorMessage = "Адрес не указан")]
-    [StringLength(70, MinimumLength = 6, ErrorMessage = "Диапазон от 6 до 70")]
-    [EmailAddress]
-    public string? Email { get; set; }
+    //[Required(ErrorMessage = "Адрес не указан")]
+    //[StringLength(70, MinimumLength = 6, ErrorMessage = "Диапазон от 6 до 70")]
+    //[EmailAddress]
+    //public string? Email { get; set; }
 
     [Required(ErrorMessage = "Возраст не указан")]
     [Range(1, 110, ErrorMessage = "Диапазон от 1 до 110")]
